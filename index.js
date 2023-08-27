0dc7d218d84c5d3b396b8d6f000bdefde1051c38
Peyton Meredith <peymeredith@gmail.com>
2023-08-27 15:33:32 -0500

    modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainElement = document.getElementById("main")
+mainElement.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.setAttribute("id", 'victory')
+newHeader.textContent = "Peyton is the champion"
+
+document.body.append(newHeader)
